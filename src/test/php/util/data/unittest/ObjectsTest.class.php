b7814c78dcce223d84788ac89acf75ada2783f14
Timm Friebe <thekid@thekid.de>
2018-08-29 16:38:51 +0200

    Verify value objects inside arrays work

--- src/test/php/util/data/unittest/ObjectsTest.class.php
+++ src/test/php/util/data/unittest/ObjectsTest.class.php
@@ -23,6 +23,14 @@ class ObjectsTest extends TestCase {
   }
 
   #[@test]
+  public function marshal_person_value_object_inside_array() {
+    $this->assertEquals(
+      [['id' => 6100, 'name' => 'Test']],
+      (new Marshalling())->marshal([new Person(6100, 'Test')])
+    );
+  }
+
+  #[@test]
   public function marshal_value_object_in_value_object() {
     $this->assertEquals(
       ['list' => [['id' => 6100, 'name' => 'Test']]],
@@ -37,15 +45,25 @@ class ObjectsTest extends TestCase {
   public function unmarshal_person_value($object) {
     $this->assertEquals(
       new Person(6100, 'Test'),
-      (new Marshalling())->unmarshal($object, Type::forName(Person::class))
+      (new Marshalling())->unmarshal($object, Person::class)
     );
   }
 
   #[@test]
   public function unmarshal_person_value_object_from_inside_map() {
+    $type= Type::forName('[:util.data.unittest.Person]');
     $this->assertEquals(
       ['person' => new Person(6100, 'Test')],
-      (new Marshalling())->unmarshal(['person' => ['id' => 6100, 'name' => 'Test']], Type::forName('[:util.data.unittest.Person]'))
+      (new Marshalling())->unmarshal(['person' => ['id' => 6100, 'name' => 'Test']], $type)
+    );
+  }
+
+  #[@test]
+  public function unmarshal_person_value_object_from_inside_array() {
+    $type= Type::forName('util.data.unittest.Person[]');
+    $this->assertEquals(
+      [new Person(6100, 'Test')],
+      (new Marshalling())->unmarshal([['id' => 6100, 'name' => 'Test']], $type)
     );
   }
 
@@ -53,7 +71,7 @@ class ObjectsTest extends TestCase {
   public function unmarshal_object_noconstructor_regression() {
     $this->assertEquals(
       (new PersonWithoutConstructor())->setId(6100)->setName('Test'),
-      (new Marshalling())->unmarshal(['id' => 6100, 'name' => 'Test'], Type::forName(PersonWithoutConstructor::class))
+      (new Marshalling())->unmarshal(['id' => 6100, 'name' => 'Test'], PersonWithoutConstructor::class)
     );
   }
 
@@ -61,7 +79,7 @@ class ObjectsTest extends TestCase {
   public function unmarshal_object_less_arguments_regression() {
     $this->assertEquals(
       (new PersonWithoutConstructor())->setId(6100),
-      (new Marshalling())->unmarshal(['id' => 6100], Type::forName(PersonWithoutConstructor::class))
+      (new Marshalling())->unmarshal(['id' => 6100], PersonWithoutConstructor::class)
     );
   }
 
@@ -70,7 +88,7 @@ class ObjectsTest extends TestCase {
     $subscribables= ['one' => 1, 'two' => 2];
     $this->assertEquals(
       (new Activity())->setSubscribables($subscribables),
-      (new Marshalling())->unmarshal(['subscribables' => $subscribables], Type::forName(Activity::class))
+      (new Marshalling())->unmarshal(['subscribables' => $subscribables], Activity::class)
     );
   }
 }
