8b67e6f98cb583a170f522ceb5065e9dfd0a65d5
Timm Friebe <thekid@thekid.de>
2024-11-01 11:33:16 +0100

    Add mapping() and resolving() to supply custom (un)marshalling logic

--- src/test/php/util/data/unittest/MarshallingTest.class.php
+++ src/test/php/util/data/unittest/MarshallingTest.class.php
@@ -3,6 +3,7 @@
 use lang\Type;
 use test\{Assert, Test, Values};
 use util\data\Marshalling;
+use util\data\unittest\fixtures\Person;
 
 class MarshallingTest {
 
@@ -29,5 +30,25 @@ class MarshallingTest {
   #[Test]
   public function unmarshal_without_type() {
     Assert::equals(1, (new Marshalling())->unmarshal(1));
+  }
+
+  #[Test]
+  public function mapping() {
+    $marshalling= (new Marshalling())->mapping(
+      Person::class,
+      fn($person) => ['id' => $person->id()]
+    );
+
+    Assert::equals(['id' => 6100], $marshalling->marshal(new Person(6100, 'Test')));
+  }
+
+  #[Test]
+  public function resolving() {
+    $marshalling= (new Marshalling())->resolving(
+      Person::class,
+      fn($value) => new Person($value['id'], 'Test')
+    );
+
+    Assert::equals(new Person(6100, 'Test'), $marshalling->unmarshal(['id' => 6100], Person::class));
   }
 }
